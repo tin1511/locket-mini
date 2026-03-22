c154f23290a0bb1f4eb0349eb496c23bba40b5f1
Nguyên Tịnh <tinbo515@outlook.com>
2026-03-22 14:52:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -134,4 +134,6 @@ def logout():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
