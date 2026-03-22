5c97d9f2a32743a84518909c7f54240f345b6125
Nguyên Tịnh <tinbo515@outlook.com>
2026-03-22 14:36:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def upload():
 def get_photos():
     return jsonify(photos)
 
-# FIX PORT CHO RENDER
+# 🔥 FIX RENDER PORT
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
